4494738a17b152cd851f2e6b7e6b19d0258019c9
Alan Rostem <42648988+AlanRostem@users.noreply.github.com>
2022-08-15 22:31:18 +0100

    Added type file and a test code file for the language itself

---
+++ Arp/Arp-language/Type.h
@@ -0,0 +1,19 @@
+//
+// Created by arost on 8/15/2022.
+//
+
+#ifndef ARP_TYPE_H
+#define ARP_TYPE_H
+
+enum ArpLang_TokenType {
+    ARPLANG_TOKENTYPE_INT,
+    ARPLANG_TOKENTYPE_FLOAT,
+    ARPLANG_TOKENTYPE_STRING,
+    ARPLANG_TOKENTYPE_DICT,
+    ARPLANG_TOKENTYPE_ARRAY,
+    ARPLANG_TOKENTYPE_FUNCTION,
+};
+
+// static const char* ARPLANG_TOKENREGEX_INT = "[0-9]";
+
+#endif //ARP_TYPE_H
