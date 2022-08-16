711dccbb22b90a6c26806155d77e6798a5c44cad
Alan Rostem <42648988+AlanRostem@users.noreply.github.com>
2022-08-16 13:23:31 +0200

    Added a compile definition to make pcre2 work

--- Arp/Arp-interpreter/library.c
+++ Arp/Arp-interpreter/library.c
@@ -3,6 +3,8 @@
 //
 
 #include "Library.h"
+
+#define PCRE2_CODE_UNIT_WIDTH 8
 #include "pcre2.h"
 
 int ArpInterpreter_interpretCode(const char *code, uint64_t length)
@@ -12,9 +14,8 @@ int ArpInterpreter_interpretCode(const char *code, uint64_t length)
     for (uint64_t i = 0; i < length; i++)
     {
         char currentChar = code[i];
-
-        // int x = pcre2_regcomp(0, 0, 0);
-
+        // PCRE2_SPTR8 sptr;
+        // pcre2_compile(sptr, );
     }
 
     return 0;
